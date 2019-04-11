8418c30e123ae994f5a089eb05de24a49db51546
Kaleb Berry <k98berry@gmail.com>
2019-04-11 10:22:42 -0400

    working library implemented

--- rnumeralWeb/src/main/java/com/example/rnumeralWeb/RnumeralWebApplication.java
+++ rnumeralWeb/src/main/java/com/example/rnumeralWeb/RnumeralWebApplication.java
@@ -1,9 +1,11 @@
 package com.example.rnumeralWeb;
 
+import edu.xavier.csci.k98berry.Library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
 public class RnumeralWebApplication {
@@ -17,8 +19,19 @@ public class RnumeralWebApplication {
 	public ResponseSingleDecode encode(@PathVariable("id") String L) {
 		ResponseSingleDecode response = new ResponseSingleDecode();
 
-		response.setNumber(rNumeralLibrary.decode(L));
+		response.setNumber(Library.decode(L));
 
 		return response;
 	}
+
+	@GetMapping("/encode/{id}")
+	public ResponseSingleEncode decode(@PathVariable("id") int num) {
+		ResponseSingleEncode response = new ResponseSingleEncode();
+
+		response.setLetter(Library.encode(num));
+
+		return response;
+	}
+
+
 }
